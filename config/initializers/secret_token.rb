b162e94cfe95029b80c238e725486bce96f26efc
Craig Maslowski <craig@Craigs-MacBook-Pro.local>
2013-03-28 11:15:38 -0700

    added empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slapstick::Application.config.secret_token = '8a56529ec47072217b559829b885f9ad978f837f4c401efb330ae1abb6f8fdb12afda14f53b05caecf2bb54ab0d3c5f8d127fb3fe5df9590b5e1219c79aabb3b'
